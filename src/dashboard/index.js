cfc723e7352700d73476bdfbe8abfc84564729e2
admin <admin@admins-MacBook-Air.local>
2022-12-22 22:14:20 +0530

    navbar

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -32,70 +32,7 @@ import Header from "./Header/HeaderPage";
 import "../CommonCss/commonClasses.css"
 import "../CommonCss/animations.css"
 
-
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
-  
 const Dashboard= () =>{
-    const [openSidebar, setSideBar] = useState(false);
-    const [openLogin, setOpenLogin] = useState(false);
-    const [openSignUp, setOpenSignUp] = useState(false);
-    const [openDashboard, setDashboard] = useState(false);
-   
-    const handleClick = (type) =>{
-        if(type === 'Log Out'){
-            sessionStorage.removeItem("user")
-        }else{
-
-        }
-        setSideBar(false)
-    }
-
-    const findItems = () =>{
-        let arr= ['Apply For Easy EMI','Log Out']
-        let userObj = JSON.parse(sessionStorage.getItem('user'));
-        if(userObj.isAdmin){
-            arr.push('Adim Dashboard');
-        }
-        if(userObj.isVerified){
-            arr.push('Dashboard');
-        }
-        return arr??[];
-    }
-
-    const list = () =>{
-        return(
-            <Box
-                sx={{ width: 250 }}
-                role="presentation"
-                onClick={()=>console.log(false)}
-                >
-                <List>
-                    {findItems().map((text) => (
-                    <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                        <ListItemText primary={text}  onClick={()=>handleClick(text)}/>    
-                        </ListItemButton>
-                    </ListItem>
-                    ))}
-                </List>
-            </Box>
-        )
-    }
-    const handleOnLoginClick = (user) =>{
-        if(!user){
-            setOpenLogin(true);
-        }
-    }
     return(
     <>
         <Header/>
